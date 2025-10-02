a47694b117927dd0ad586898eff401fc5457ec06
Dr. Deepak Hirenwal <deepakhirenwal@gmail.com>
2025-10-02 20:31:32 +0200

    initial project setup: - Spring boot with Java 25, docker compose yaml - added langchain4j, webflux, and other required dependencies

---
+++ src/test/java/com/deepak/ragchatbot/RagChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.ragchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
